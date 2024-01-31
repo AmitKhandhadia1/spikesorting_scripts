eeb6620e2ba8f46012b5394a4f90ec54a8008382
Amit Khandhadia <khandhadiaamit@gmail.com>
2024-01-31 11:33:27 +0000

    Adapting for MCS

--- spikesorting_scripts/helpers.py
+++ spikesorting_scripts/helpers.py
@@ -47,6 +47,25 @@ def generate_warp_32ch_probe():
 
     probe.set_device_channel_indices(channel_indices - 1)
 
+def generate_warp_32ch_probeMCS():
+    probe = generate_multi_columns_probe(num_columns=8,
+                                         num_contact_per_column=4,
+                                         xpitch=350, ypitch=350,
+                                         contact_shapes='circle')
+    probe.create_auto_shape('rect')
+
+    channel_indices = np.array([29, 31, 30, 32,
+                                25, 27, 26, 28,
+                                21, 23, 22, 24,
+                                17, 19, 18, 20,
+                                15, 13, 16, 14,
+                                11, 9, 12, 10,
+                                7, 5, 8, 6,
+                                3, 1, 4, 2])
+
+    probe.set_device_channel_indices(channel_indices - 1)
+
+
     return probe
 
 def sort_np_sessions(
